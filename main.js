65ab5bd611500dd3bea6106d8bb2ce5c30c49295
Octavian95 <Octavian.val95@gmail.com>
2019-05-30 00:29:37 +0300

    New script added

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 
 aler('file linked');
 
+<script>
+function myF() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+</script>
